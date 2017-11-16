86450db29a87891766568708a8517c61cf28bff8
Grégory <gregory.bouloc@gmail.com>
2017-11-16 15:21:05 +0100

    exercice 5 terminé

--- array/app.js
+++ array/app.js
@@ -4,10 +4,21 @@ var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 var mois = ['janvier','février','mars','avril','juin','juillet','aout','septembre','octobre','novembre','décembre'];
 
 // Exercice 2
-
 for(var i=0; i < 12 ; i++ ){
     if ( i === 2 ){
-    		console.log(mois[i]);
+    		console.log("Affihcage de la ligne 3: " + mois[i]);
     }
 
 }
+
+// Exercice 3
+console.log("Affichage de l'index 5: " + mois[5]);
+
+// Exercice 4
+console.log("Affichage du mois de Novembre : " + mois[9]);
+
+//Exercice 5
+mois[8] = "Août";
+console.log("Exercice 5: " + mois[8]);
+
+// 
