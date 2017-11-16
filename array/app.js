f72e604e27102d0de9e75a736a0c22c568b80d7e
Grégory <gregory.bouloc@gmail.com>
2017-11-16 14:55:35 +0100

    Exercie 2 fini

--- array/app.js
+++ array/app.js
@@ -1,4 +1,13 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
-
+// Exercice 1
 var mois = ['janvier','février','mars','avril','juin','juillet','aout','septembre','octobre','novembre','décembre'];
+
+// Exercice 2
+
+for(var i=0; i < 12 ; i++ ){
+    if ( i === 2 ){
+    		console.log(mois[i]);
+    }
+
+}
